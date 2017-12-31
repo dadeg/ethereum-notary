c82f892175c7fa6582ebaea05276eddabfc73586
Dan DeGreef <daniel_degreef@condenast.com>
2017-12-31 11:20:19 -0600

    update app to have a dropzone and to properly hash documents

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import contract from 'truffle-contract';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 
+import Dropzone from 'react-dropzone';
+
 let storeInstance;
 
 function getInstance() {
@@ -31,33 +33,107 @@ function createStoreInstance() {
   return storeContract.deployed();
 }
 
+/**
+ * @param {ArrayBuffer} buffer
+ */
+function sha256(buffer) {
+  return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
+    return hex(hash);
+  });
+}
+
+function hex(buffer) {
+  var hexCodes = [];
+  var view = new DataView(buffer);
+  for (var i = 0; i < view.byteLength; i += 4) {
+    // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+    var value = view.getUint32(i)
+    // toString(16) will give the hex representation of the number without padding
+    var stringValue = value.toString(16)
+    // We use concatenation and slice for padding
+    var padding = '00000000'
+    var paddedValue = (padding + stringValue).slice(-padding.length)
+    hexCodes.push(paddedValue);
+  }
+
+  // Join all the hex strings into one
+  return hexCodes.join("");
+}
+
+
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { foundEntries: [] };
+
+    this.state = {
+      foundEntries: [],
+      signer: '',
+      documentHash: '',
+      documentHashToUpload: ''
+    };
+
+    this.createEntry = this.createEntry.bind(this);
     this.findEntries = this.findEntries.bind(this);
+    this.getSetter = this.getSetter.bind(this);
+    this.onDrop = this.onDrop.bind(this);
   }
 
   createEntry() {
+    if (!this.state.documentHashToUpload) { return; }
     return getInstance().then(instance => {
-      return Promise.resolve(instance.create('12345666'));
+      console.log(this.state.documentHashToUpload);
+      return Promise.resolve(instance.create("0x" + this.state.documentHashToUpload)); // adding 0x because it is needed for Solidity to recognize the hash as byte32
     });
   }
 
   findEntries() {
-    this.setState(prevState => {
-      return Object.assign({}, prevState, { foundEntries: [] });
-    });
+    this.setState({ foundEntries: [] });
 
     getInstance().then(instance => {
-      const entryEvent = instance.Entry({}, { fromBlock: 0, toBlock: 'latest' });
+      const filters = {};
+      if (this.state.signer) {
+        filters.signer = this.state.signer;
+      }
+      if (this.state.documentHash) {
+        filters.documentHash = "0x" + this.state.documentHash;
+      }
+
+      let entryEvent;
+      try {
+        entryEvent = instance.Entry(filters, { fromBlock: 0, toBlock: 'latest' });
+      } catch (e) {
+        console.log(e);
+        entryEvent = instance.Entry({}, { fromBlock: 0, toBlock: 'latest' });
+      }
+
       entryEvent.watch((err, response) => {
         this.setState(prevState => {
+          response.args.documentHash = response.args.documentHash.slice(2); // Knock off the 0x.
           prevState.foundEntries.push(response.args);
           return prevState;
         });
       });
     });
+  }
+
+  getSetter(property) {
+    return event => {
+      this.setState({ [property]: event.target.value });
+    };
+  }
+
+  onDrop(files) {
+    const file = files[0]; // Only take the first file.
+    const reader = new FileReader();
+    reader.onload = () => {
+      const buffer = reader.result;
+      sha256(buffer).then(hash => this.setState({ documentHashToUpload: hash }));
+    };
+    reader.onabort = () => console.log('file reading was aborted');
+    reader.onerror = () => console.log('file reading has failed');
+
+    reader.readAsArrayBuffer(file);
+
   }
 
   render() {
@@ -68,7 +144,7 @@ class App extends Component {
     }, {
       Header: 'Document Hash (SHA512)',
       accessor: 'documentHash'
-    }]
+    }];
 
     return (
       <div className="App">
@@ -76,10 +152,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.createEntry}>Create Entry</button>
+        <Dropzone onDrop={this.onDrop}>
+          <p>Try dropping some files here, or click to select files to upload.</p>
+        </Dropzone>
+        <p>Uploaded Document's Hash: {this.state.documentHashToUpload || "upload a document first"}</p>
+        <button onClick={this.createEntry}>Create Entry with Document Hash</button>
+        <label>
+          Signer (Address):
+          <input type="text" value={this.state.signer} onChange={this.getSetter('signer')} />
+        </label>
+        <label>
+          Document Hash (SHA-256):
+          <input type="text" value={this.state.documentHash} onChange={this.getSetter('documentHash')} />
+        </label>
         <button onClick={this.findEntries}>Find Entries</button>
         <ReactTable
           data={this.state.foundEntries}
